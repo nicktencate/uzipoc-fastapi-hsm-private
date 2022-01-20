4be3110a2ecfeddb006bffe168b5fa764204ca8d
Max Vasterd <vasterd97@gmail.com>
2022-01-20 15:43:22 +0100

    removed old tests

--- tests/genaes.py
+++
@@ -1,4 +0,0 @@
-def test(session, baseurl):
-    params = {"bits": 256, "label": "AESkey"}
-    print("Generating AES key: ", params["bits"])
-    return session.post(baseurl + "/generate/aes", json=params).json()
--- tests/genec.py
+++
@@ -1,4 +0,0 @@
-def test(session, baseurl):
-    params = {"curve": "secp256r1", "label": "ECkey"}
-    print("Generating EC key: ", params["curve"])
-    return session.post(baseurl + "/generate/ec", json=params).json()
