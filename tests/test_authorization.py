864631ef17511e357655905f9e9bc9ad4d2cbccf
Mendel Mobach <mendel@mobach.nl>
2022-01-31 14:57:31 +0100

    lint

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,4 +1,3 @@
-from fastapi.exceptions import HTTPException
 from base64 import b64encode
 
 import asn1crypto.keys
@@ -7,16 +6,32 @@ import asn1crypto.x509
 import pytest
 
 import tests.certgen
+
+
 def makecert(name):
-    asn1pk = asn1crypto.keys.PublicKeyInfo({ "algorithm": {"algorithm": "rsa", "parameters": None}, "public_key": {'modulus': 0, 'public_exponent': 0} })
-    thecert = asn1crypto.x509.Certificate({'tbs_certificate': tests.certgen.certgen(name, asn1pk, {"algorithm": 'md5_rsa'}) , 'signature_value': b'\x00' , 'signature_algorithm': {'algorithm': 'md5_rsa'}})
+    asn1pk = asn1crypto.keys.PublicKeyInfo(
+        {
+            "algorithm": {"algorithm": "rsa", "parameters": None},
+            "public_key": {"modulus": 0, "public_exponent": 0},
+        }
+    )
+    thecert = asn1crypto.x509.Certificate(
+        {
+            "tbs_certificate": tests.certgen.certgen(
+                name, asn1pk, {"algorithm": "md5_rsa"}
+            ),
+            "signature_value": b"\x00",
+            "signature_algorithm": {"algorithm": "md5_rsa"},
+        }
+    )
     return b64encode(thecert.dump()).decode()
+
 
 @pytest.mark.enable_authorization_middleware
 def test_successful_authorization(client, module, slot):
     # Some random call that requires authentication:
     params = {"bits": 256, "label": "AESkey42"}
-    headers = {"x-ssl-cert": makecert(f'{module}^{slot}^*=create')}
+    headers = {"x-ssl-cert": makecert(f"{module}^{slot}^*=create")}
     resp = client.post(
         f"/hsm/{module}/{slot}/generate/aes", json=params, headers=headers
     ).json()
@@ -27,7 +42,7 @@ def test_successful_authorization(client, module, slot):
 def test_unsuccessful_authorization(client, module, slot):
     # Some random call that requires authentication:
     params = {"bits": 256, "label": "AESkey42"}
-    headers = {"x-ssl-cert": makecert(f'{module}^{slot}^*=use')}
+    headers = {"x-ssl-cert": makecert(f"{module}^{slot}^*=use")}
     assert client.post(
-            f"/hsm/{module}/{slot}/generate/aes", json=params, headers=headers
-        ).json() == {'detail': 'Not authorized for usage: create'}
+        f"/hsm/{module}/{slot}/generate/aes", json=params, headers=headers
+    ).json() == {"detail": "Not authorized for usage: create"}
